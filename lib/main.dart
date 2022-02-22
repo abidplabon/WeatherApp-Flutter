4a8b144befd1feb10a6027c9610d392d9211e7ea
abidplabon <mohammedplabon@gmail.com>
2022-02-22 11:14:33 -0800

    Failed trial of undefined list hourly

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:weatherforecast/currentWeather.dart';
+import 'package:weatherforecast/models/location.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  List<Location> locations = [
+    new Location(city: "calgary", country: "canada", lat: "51.0407154", lon: "-114.1513999")
+  ];
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CurrentWeatherPage(),
+      home: CurrentWeatherPage(locations),
     );
   }
 }
--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -4,16 +4,25 @@ class Weather {
   final double? low;
   final double? high;
   final String? description;
-//by making Constructor we can now call different variables that we've declared up
-  Weather({this.temp, this.feelsLike, this.low, this.high, this.description});
-//factory will get and connect all the json related data conversion and interactions when required
+  final double? pressure;
+  final double? humidity;
+  final double? wind;
+  final String? icon;
+
+  Weather({this.temp, this.feelsLike, this.low, this.high, this.description, this.pressure, this.humidity, this.wind, this.icon});
+
   factory Weather.fromJson(Map<String, dynamic> json) {
+    print(json);
     return Weather(
       temp: json['main']['temp'].toDouble(),
       feelsLike: json['main']['feels_like'].toDouble(),
       low: json['main']['temp_min'].toDouble(),
       high: json['main']['temp_max'].toDouble(),
       description: json['weather'][0]['description'],
+      pressure: json['main']['pressure'].toDouble(),
+      humidity: json['main']['humidity'].toDouble(),
+      wind: json['wind']['speed'].toDouble(),
+      icon: json['weather'][0]['icon'],
     );
   }
 }
